09b8f8e33956b69aa97e61b0ba58a2ef5058fc7a
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2026-03-11 16:44:13 +0700

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(200).end()
   if (req.method !== 'POST') return res.status(405).end()
 
-  const webhookUrl = process.env.DISCORD_WEBHOOK
+  const webhookUrl = process.env.NEXT_PUBLIC_ANALYTICS_ID
 
   if (!webhookUrl) return res.status(500).json({ ok: false })
 
