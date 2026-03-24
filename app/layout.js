89a95eab92efda99a0e164e92a39fcea7e925efb
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2026-03-24 23:45:43 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Sambung Kata - Givyx',
+  description: 'Kamus referensi untuk map Roblox Sambung Kata',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
