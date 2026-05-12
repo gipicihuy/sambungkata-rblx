8c921bdf768709c8ea0b7c2c15c6fc4e30005dae
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2026-05-12 09:54:49 +0700

    [rozi] Hapus 1 kata: pneumonoultramikroskopisilikovolkanokoliosis

--- src/db.js
+++ src/db.js
@@ -85041,7 +85041,6 @@ export default [
   "pneumonik",
   "pneumonitis",
   "pneumonoultramicroscopicsilicovolcanoconiosis",
-  "pneumonoultramikroskopisilikovolkanokoliosis",
   "pneumosistis",
   "png",
   "pnigerofobia",
