c4ca12640dd771ce6990276b59c40d581367b32b
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2026-05-12 09:55:06 +0700

    [rozi] Tambah 1 kata: pneumonoultramikroskopisilikovolkanokoniosis

--- src/db.js
+++ src/db.js
@@ -85041,6 +85041,7 @@ export default [
   "pneumonik",
   "pneumonitis",
   "pneumonoultramicroscopicsilicovolcanoconiosis",
+  "pneumonoultramikroskopisilikovolkanokoniosis",
   "pneumosistis",
   "png",
   "pnigerofobia",
